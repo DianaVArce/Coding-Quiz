4badacf1721ce8470c2e479610e95688d0aeedd9
DianaVArce <60335621+DianaVArce@users.noreply.github.com>
2021-08-18 21:15:43 -0700

    Updated comments
    Updated comments to the final commit for the score.js page as to better explain the functions and even listeners.

--- Assets/js/scores.js
+++ Assets/js/scores.js
@@ -1,24 +1,25 @@
+//Create variables for the queary slectors buttons
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 
-// Event listener to clear scores 
+// Add an Event listener to clear scores 
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
-// Retreives local stroage 
+
+// Retreives local storage for the allocated scores on script.js
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
+//as long as there are scores to get
 if (allScores !== null) {
-
+    //populate the high-scores page
     for (var i = 0; i < allScores.length; i++) {
-
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " " + allScores[i].score;
         highScore.appendChild(createLi);
-
     }
 }
 // Event listener to move to index page
