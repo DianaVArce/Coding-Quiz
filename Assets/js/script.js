7a62209753d559ccf188f084bade084f136795a7
DianaVArce <60335621+DianaVArce@users.noreply.github.com>
2021-08-18 21:49:31 -0700

    Updated the style sheet
    Played around with the style sheet to create more complimentary colors with the background image.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,13 +22,13 @@ var questions = [
     },
     {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        choices: ["C++", "bash", "for loops", "console log"],
         answer: "console log"
     },
     {        
         title: "Which event occurs when the user clicks on an HTML element?",
-        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-        answer: "a. onclick"
+        choices: ["a. on-click", "b. nothing", "c. on-mouseover", "d. on-mouseclick"],
+        answer: "a. on-click"
     },
 
 ];
